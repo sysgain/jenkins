1570ef288903555f6088daa22b6e4f6e46ed84b5
ooha-m <omanduri@sysgain.com>
2019-07-15 17:07:33 +0530

    Create install_jenkins.sh

---
+++ scripts/install_jenkins.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install default-jre -y
+sudo apt-get install default-jdk -y
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins
+sudo apt-get install git-core
